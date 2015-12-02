de66dd37e4c2e03da72ccf5f2ff961632286ed7d
Alexander <alexgo25616@gmail.com>
2015-12-03 01:23:16 +0300

    ooh

---
+++ Codebusters_sample/Kids'n'Code-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Kids'n'Code-Bridging-Header.h
+//  Kids'n'Code
+//
+//  Created by Alexander on 03/12/15.
+//  Copyright © 2015 Kids'n'Code. All rights reserved.
+//
+
+#ifndef Kids_n_Code_Bridging_Header_h
+#define Kids_n_Code_Bridging_Header_h
+
+#import "FMDB.h"
+#import "YandexMobileMetrica.h"
+
+#endif /* Kids_n_Code_Bridging_Header_h */
